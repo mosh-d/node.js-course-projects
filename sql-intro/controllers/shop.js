58dcbb86492deff9821c08376d3a9aaaae5d0a28
mosh-d <shopsy8@gmail.com>
2025-08-28 17:09:46 +0100

    Updating controllers and models with sequelize

--- sql-intro/controllers/shop.js
+++ sql-intro/controllers/shop.js
@@ -2,25 +2,25 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      res.render("shop/product-list", {
-        prods: rows,
+  Product.findAll()
+    .then((products) => {
+       res.render("shop/product-list", {
+        prods: products,
         pageTitle: "All Products",
         path: "/products",
       });
     })
     .catch((err) => {
-      console, log(err);
+      console.log(err);
     });
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId)
-    .then(([product]) => {
+    .then((product) => {
       res.render("shop/product-detail", {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: "/products",
       });
@@ -31,10 +31,10 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render("shop/index", {
-        prods: rows,
+        prods: products,
         pageTitle: "Shop",
         path: "/",
       });
